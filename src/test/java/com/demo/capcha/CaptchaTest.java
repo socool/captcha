8b97758dd40dd1e9225257c21bd9cbb54290858e
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2019-02-27 19:52:34 +0700

    implement get/set

--- src/test/java/com/demo/capcha/CaptchaTest.java
+++ src/test/java/com/demo/capcha/CaptchaTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class CaptchaTest {
     @Test
     public void firstPatternLeftOperandShouldBe1(){
-        Captcha captcha =new Captcha();
+        Captcha captcha = new Captcha(1,1,1,1);
         Assert.assertEquals("1 + One",captcha.toString());
     }
 
